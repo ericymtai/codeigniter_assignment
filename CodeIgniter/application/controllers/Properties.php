2318bba99e58fbeb9ca750f5cb7f5d30ec654e7e
eric tai <ericymtai@gmail.com>
2016-12-13 16:33:16 +0000

    clean up version

--- CodeIgniter/application/controllers/Properties.php
+++ CodeIgniter/application/controllers/Properties.php
@@ -3,8 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Properties extends CI_Controller {
 
-	//only this controller needed!!!! IMPORTANT
 	
+	//only this controller needed!!!! IMPORTANT
 		public function index()
 		{
 					
@@ -34,61 +34,9 @@ class Properties extends CI_Controller {
 				$this->load->view('layouts/header');
 				$data['name'] = $this->Property->get();
         $this->load->view('properties/index', $data);
-			
-			
+				$this->load->view('layouts/footer');
 			
 		}
-    
-//     public function upload_form()
-//     { 
-//         $this->load->model('Property');
-
-//         $data['name'] = $this->Property->get();
-//     		$this->load->view('properties/upload_form', $data);
-// 		}
-	
-//      public function thank()
-//     {
-// 				$error = ['error'=> ''];   
-// 				$data = [];
-// 				$config['upload_path'] = './uploads/';
-//         $config['allowed_types']  = 'gif|jpg|png';
-       
-              
-
-//         $this->load->library('upload', $config);
-				
-			
-// 				if($this->input->method() == 'post'){
-
-// 					if ( ! $this->upload->do_upload('uploadFile'))
-// 					 {
-// 						$error = array('error' => $this->upload->display_errors());
-
-// 					 }
-// 					else
-// 					{
-// 						$data['uploadedImage'] = $this->upload->data();
-// 					}
-// 				}
-				
-//         $this->load->helper(array('form', 'url'));
-// 				$this->load->model('Property');
-// 				$this->load->view('layouts/header');
-// 				$data['name'] = $this->Property->get();
-// //        	$this->load->view('properties/uploadPreview', $error);
-//         $this->load->view('properties/thank', $data);
-			
-			
-			
-
-//     }
-    
-    
-//     public function db_test()
-//    {
-//       $this->load->model('Property');
-//       $this->Property->connection_test();
-//    }
+ 
 }
 
